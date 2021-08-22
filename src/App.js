50fd03725dcca72c25e6c01e8473e7ddecf4aea4
Alon Nusem <61749872+4lon@users.noreply.github.com>
2021-08-23 02:11:33 +1000

    Start of Navbar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 import About from './About';
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 function App() {
     return (
         <Router>
             <div>
-                <div className="menu">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                </div>
+                {/*<div className="menu">*/}
+                {/*    <Link to="/">Home</Link>*/}
+                {/*    <Link to="/about">About</Link>*/}
+                {/*</div>*/}
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="/">
+                            <img
+                                alt=""
+                                src="/logo.svg"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                            React Bootstrap
+                        </Navbar.Brand>
+                    </Container>
+                </Navbar>
                 <Switch>
                     <Route exact path="/">
                         <Home/>
